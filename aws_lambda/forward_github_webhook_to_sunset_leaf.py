c3bbe5dc258f38c062e1d26014ff6e7c9fb7718d
Austin Spafford <austin.spafford@gmail.com>
2016-01-11 20:27:02 -0800

    Removing the signature-validation attempt
    Rejecting the signature-validation code, but keeping all the other
    improvements.

--- aws_lambda/forward_github_webhook_to_sunset_leaf.py
+++ aws_lambda/forward_github_webhook_to_sunset_leaf.py
@@ -9,31 +9,7 @@ def get_required_parameter(
         raise Exception("The caller must specify the '" + parameter_name + "' parameter in the API-gateway's integration-request's mapping-templates.")
     
     return event[parameter_name]
-    
 
-def validate_request_signature(
-    incoming_request_signature, 
-    request_body, 
-    github_secret):
-    
-    print '\n', "incoming_request_signature", '\n', incoming_request_signature, '\n'
-    
-    # The API gateway parsed the JSON, now we need to precisely undo it
-    github_formatted_request_body = json.dumps(obj = request_body, indent = 2)
-    
-    # print '\n', "github_formatted_request_body", '\n', github_formatted_request_body, '\n'
-   
-    expected_request_signature = "sha1=" + hmac.new(
-        key = str(github_secret), 
-        msg = str(github_formatted_request_body), 
-        digestmod = hashlib.sha1).hexdigest()
-    
-    print '\n', "expected_request_signature", '\n', expected_request_signature, '\n'
-	
-	# TODO: Call hmac.compare_digest(), and raise-exception if unequal.
-	
-    return None
-	
 
 def webhook_handler(
 	event, 
@@ -45,9 +21,6 @@ def webhook_handler(
     particle_access_token = get_required_parameter(event, 'particle_access_token')
     
     incoming_request_body = get_required_parameter(event, 'request_body')
-    incoming_request_signature = get_required_parameter(event, 'request_signature')
-    
-    validate_request_signature(incoming_request_signature, incoming_request_body, github_secret)
     
     outgoing_request_querystring = urllib.urlencode({
         'access_token': particle_access_token,
@@ -55,13 +28,14 @@ def webhook_handler(
         
     outgoing_request_headers = {
         "content-type": "application/json",
-        "accept": "text/plain"}
+        "accept": "text/plain",
+		}
         
     outgoing_request_body = json.dumps({
         'name': 'web_alert',
         'data': 'github_activity',
-        'private': 'false'
-        })
+        'private': 'false',
+		})
     
     print '\n', "outgoing_request_body", '\n', outgoing_request_body, '\n'
     
